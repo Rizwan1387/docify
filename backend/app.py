30bf7b813f421e1c0feb9fe53bb1e84c2f769f1e
Abdul Mohammed <abdulbaseer657@gmail.com>
2023-09-24 02:38:59 -0500

    cors

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 import openai
 import requests
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)
 
 # defining app route
 
